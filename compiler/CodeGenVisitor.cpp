fa73a66296c835d6db35177cd5e196abe03ef533
Spiky-73 <cocojeanne73@gmail.com>
2025-03-10 11:06:36 +0100

    added main stack

--- compiler/CodeGenVisitor.cpp
+++ compiler/CodeGenVisitor.cpp
@@ -2,12 +2,18 @@
 
 antlrcpp::Any CodeGenVisitor::visitProg(ifccParser::ProgContext *ctx) 
 {
-    std::cout<< ".globl main\n" ;
-    std::cout<< " main: \n" ;
-
-    this->visit( ctx->return_stmt() );
+    std::cout << ".globl main\n";
+    std::cout << "main:\n";
+    std::cout << "\t# prologue:\n";
+    std::cout << "\tpushq %rbp # save %rbp on the stack\n";
+    std::cout << "\tmovq %rsp, %rbp # define %rbp for the current function\n\n";
     
-    std::cout << "    ret\n";
+    std::cout << "\t# body:\n";
+    this->visit(ctx->return_stmt());
+
+    std::cout << "\n\t# epilogue:\n";
+    std::cout << "\tpopq %rbp # restore %rbp from the stack\n";
+    std::cout << "\tret # return to the caller (here the shell)" << std::endl;
 
     return 0;
 }
@@ -17,7 +23,7 @@ antlrcpp::Any CodeGenVisitor::visitReturn_stmt(ifccParser::Return_stmtContext *c
 {
     int retval = stoi(ctx->CONST()->getText());
 
-    std::cout << "    movl $"<<retval<<", %eax\n" ;
+    std::cout << "\tmovl $"<<retval<<", %eax\n" ;
 
     return 0;
 }
